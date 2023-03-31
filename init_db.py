ad14c06537323757e7af86fa8aad0c278b7ca67f
Mason <47919925+OAOEnder@users.noreply.github.com>
2023-03-31 09:40:44 -0500

    Initial commit

---
+++ init_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cursor = connection.cursor()
+
+cursor.execute("INSERT INTO Users(fname, lname, email) VALUES (?, ?, ?)", ('Mason', 'Krause', 'masongkrause@yahoo.com'))
+cursor.execute("INSERT INTO Users (fname, lname, email) VALUES(?, ?, ?)", ('Tommy', 'Jones', 'tj@gmail.com'))
+
+connection.commit()
+connection.close()
