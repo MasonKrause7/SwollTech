7cf765c19eb614108159cbea15df484c25da8a76
Mason <47919925+OAOEnder@users.noreply.github.com>
2023-04-05 00:49:46 -0500

    login and registration mostly working

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, flash, request, session, redirect, url_for, flash
 import sqlite3
 
 app = Flask('SwollTech')
 DATABASE = 'database.db'
 app.secret_key = 'testingkey,changelater'
+
+
 def get_db():
     db = sqlite3.connect(DATABASE)
     db.row_factory = sqlite3.Row
@@ -12,6 +14,8 @@ def get_db():
 
 @app.route('/')
 def index():
+    if user_authenticated():
+        return render_template('home.html')
     return render_template('index.html')
 
 
@@ -21,8 +25,8 @@ def about():
 
 
 @app.get('/signup.html')
-def get_signup():
-    return render_template('signup.html')
+def get_signup(message=None):
+    return render_template('signup.html', message=message)
 
 
 @app.post('/signup.html')
@@ -32,7 +36,8 @@ def post_signup():
     confPassword = request.form['confpass']
     if (password != confPassword):
         message = "Password and confirmation do not match, please try again"
-        return render_template(url_for('login'), message=message)
+        flash(message, 'warnings')
+        return render_template(url_for('get_signup'), message=message)
     fname = request.form['fname']
     lname = request.form['lname']
     email = request.form['email']
@@ -46,10 +51,11 @@ def post_signup():
     if len(users) > 0:  # if user already exists, render login
         user = users[0]
         message = "That email is already associated with an account, please log in"
-        return render_template('login.html', user=user, message=message)
+        return render_template('login.html', message=message)
     else:
         message = ""
         try:
+            #NEED TO ENCRYPT PASSWORD HERE
             query = f"INSERT INTO Users (fname, lname, email, dob, password) VALUES ('{fname}', '{lname}', '{email}', '{dob}', '{password}')"
             cursor.execute(query)
             db.commit()
@@ -61,24 +67,15 @@ def post_signup():
             message = "There was an error during the sign up process. Please try again."
             print('error inserting record')
         finally:
-            user = None
-            if successfulInsert:
-                query = f"SELECT * FROM Users WHERE email = '{email}'"
-                results = cursor.execute(query)
-                result = results.fetchall()
-                user = result[0]
-                db.close()
+            db.close()
 
-            else:
-                db.close()
-
-            return render_template(url_for('login'), user=user, message=message)
+        return render_template(url_for('login'), message=message)
 
 
 @app.route('/login.html', methods=['GET', 'POST'])
 def login(user=None, message=""):
     if request.method == 'GET':
-        return render_template('login.html', user=user, message=message)
+        return render_template('login.html', message=message)
     else:
         username = request.form.get('username')
         password = request.form.get('loginpassword')
@@ -95,23 +92,30 @@ def login(user=None, message=""):
             user = users[0]
             passwordInDb = user['password']
             if passwordInDb == password:
+                session['logged_in'] = True
                 session['user_id'] = user['user_id']
                 session['email'] = user['email']
                 session['fname'] = user['fname']
                 session['lname'] = user['lname']
-                return render_template(url_for('home'))
+                return redirect(url_for('home'))
             else:
                 message = "Sorry, that password is incorrect."
-                return render_template(url_for('login'), user=user, message=message)
+                return render_template(url_for('login'), message=message)
+
+
 @app.route('/logout')
 def logout():
-    if session['user_id'] is None:
+    if len(session.keys()) is False:
         message = 'You were not logged in'
         return render_template(url_for('login'), message=message)
+    session.clear()
+    message = 'Successfully signed out'
+    return render_template(url_for('login'), message=message)
+
 
 @app.route('/home.html')
 def home():
-    if session['user_id'] is None:
+    if not user_authenticated():
         message = 'You must be logged in to access your home page'
         return render_template(url_for('login'), message=message)
     # query relevant homepage data
@@ -132,3 +136,9 @@ def submitworkoutname(workout_name1):
     connection = get_db()
     cursor = connection.cursor()
     cursor.execute('INSERT INTO Workout (workout_name) VALUES (workout_name1)')
+
+
+def user_authenticated() -> bool:
+    if session.get('user_id') is None:
+        return False
+    return True
